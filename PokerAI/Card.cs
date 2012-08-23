d7c2dfeb19e9ed151d7f67b34d7d87b40ceda575
Julius Buset Asplin <julius_asplin@hotmail.com>
2012-08-23 19:32:08 +0200

    LOL

---
+++ PokerAI/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokerAI
+{
+    enum Suit
+    {
+        SPADES, HEARTS, CLOVES, DIAMONDS
+    }
+    
+    class Card
+    {
+        public Suit suit;
+        public int Value;
+    }
+}
