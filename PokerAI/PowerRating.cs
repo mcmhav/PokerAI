7992bbb6edee2a7b2f26b3e4482cc9fe1d8fc532
Julius Buset Asplin <julius_asplin@hotmail.com>
2012-09-15 12:23:59 +0200

    dfg

--- PokerAI/PowerRating.cs
+++ PokerAI/PowerRating.cs
@@ -16,30 +16,59 @@ namespace PokerAI
 
         public PowerRating(List<Card> hand)
         {
-            bool flush = true;
-            Suit suit = hand[0].Suit;
 
-            List<int> valueCount = new List<int>(13)
-            {
-                0,0,0,0,0,0,0,0,0,0,0,0,0
-            };
+            List<int> valueCount = new List<int>(13){0,0,0,0,0,0,0,0,0,0,0,0,0};
+            List<int> suitCount = new List<int>(4){0,0,0,0};
+
             foreach (Card c in hand)
             {
-                if (c.Suit != suit) flush = false;
-                valueCount[c.Value]++;
+                suitCount[(int)c.Suit]++;
+                valueCount[Math.Abs(c.Value-12)]++;
+            }
+            bool flush = suitCount.Max() >= 5;
+            int straightCount = 0;
+            int highest;
+            bool straight = false;
+            for (int i = 12; i >= 0; i--)
+            {
+                if (valueCount[i] > 0)
+                {
+                    straightCount++;
+                    if (straightCount == 5)
+                    {
+                        straight = true;
+                        highest = i+4;
+                        break;
+                    }
+                }
+                else straightCount = 0;
+            }
+            if (straightCount >= 4 && valueCount[12] > 0)
+            {
+                straight = true;
+                highest = 12;
+            }
+            
+            if (flush & straight)
+            {
+                // TODO Check for straight flush.
+                first = 8;
+                second = highest;
+                return;
             }
 
-            int best = valueCount.Max();
-            switch (best)
+            int highestValueQuantity = valueCount.Max();
+            
+            switch (highestValueQuantity)
             {
                 case 4:
                     first = 7;
-                    second = valueCount.IndexOf(4);
-                    third = valueCount.IndexOf(1);
+                    second = Math.Abs(valueCount.IndexOf(4) - 12);
+                    third = Math.Abs(Math.Min(Math.Min(valueCount.IndexOf(3), valueCount.IndexOf(2)), valueCount.IndexOf(1)) - 12);
                     break;
                 case 3:
-                    second = valueCount.IndexOf(3);
-                    int nextBest = valueCount.IndexOf(2);
+                    second = Math.Abs(valueCount.IndexOf(3) - 12);
+                    int Math.Abs(Math.Min(nextBest = valueCount.IndexOf(2);
                     if (nextBest != -1)
                     {
                         first = 6;
@@ -117,18 +146,21 @@ namespace PokerAI
                     break;
             }
         }
-    }
-    
-    class Algorithms
-    {
-        public static void PreflopPowerRating(List<Card> hand)
-        {
 
+        public static int betterThan(PowerRating pr1, PowerRating powerRating2)
+        {
+            if (pr1.first != powerRating2.first) return pr1.first > powerRating2.first ? 1 : -1;
+            else if (pr1.second != powerRating2.second) return pr1.second > powerRating2.second ? 1 : -1;
+            else if (pr1.third != powerRating2.third) return pr1.third > powerRating2.third ? 1 : -1;
+            else if (pr1.fourth != powerRating2.fourth) return pr1.fourth > powerRating2.fourth ? 1 : -1;
+            else if (pr1.fift != powerRating2.fift) return pr1.fift > powerRating2.fift ? 1 : -1;
+            else if (pr1.sixth != powerRating2.sixth) return pr1.sixth > powerRating2.sixth ? 1 : -1;
+            else return 0;
         }
 
-        public static void PowerRating(List<Card> hand, List<Card> communityCards)
+        public static int betterThan(List<Card> hand)
         {
-            
+
         }
     }
 }
